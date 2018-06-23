62c845bcbd6d55d0f151f65dd80d1e48ef1bbbde
Tamino Martinius <dev@zaku.eu>
2018-06-23 11:29:41 +0200

    Add typescript decorator

---
+++ src/components/HelloDecorator.ts
@@ -0,0 +1,30 @@
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({
+  template: `
+    <div>
+      <div>Hello {{name}}{{exclamationMarks}}</div>
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
+  `,
+})
+export default class HelloDecorator extends Vue {
+  @Prop() name!: string;
+  @Prop() initialEnthusiasm!: number;
+
+  enthusiasm = this.initialEnthusiasm;
+
+  increment() {
+    this.enthusiasm += 1;
+  }
+  decrement() {
+    if (this.enthusiasm > 1) {
+      this.enthusiasm -= 1;
+    }
+  }
+
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join('!');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import vue from 'vue';
 import Hello from './components/Hello';
+import HelloDecorator from './components/HelloDecorator';
 import HelloVue from './components/Hello.vue';
 import HelloDecoratorVue from './components/HelloDecorator.vue';
 
@@ -10,6 +11,8 @@ const v = new vue({
       Name: <input v-model="name" type="text">
       <h1>Hello TS Component</h1>
       <hello :name="name" :initialEnthusiasm="5" />
+      <h1>Hello TS Decorator Component</h1>
+      <hello-decorator :name="name" :initialEnthusiasm="5" />
       <h1>Hello Vue Component</h1>
       <hello-vue :name="name" :initialEnthusiasm="5" />
       <h1>Hello Vue Decorator Component</h1>
@@ -19,6 +22,7 @@ const v = new vue({
   data: { name: 'World' },
   components: {
     Hello,
+    HelloDecorator,
     HelloVue,
     HelloDecoratorVue,
   },
