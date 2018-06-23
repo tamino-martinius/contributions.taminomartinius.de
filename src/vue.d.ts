bc69aa92b25d2c383026c28704b695ba84189bd1
Tamino Martinius <dev@zaku.eu>
2018-06-23 11:29:48 +0200

    Change quotes

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,5 +1,5 @@
 // See: https://github.com/prograhammer/vscode-tslint-vue
-declare module "*.vue" {
-  import Vue from "vue";
+declare module '*.vue' {
+  import Vue from 'vue';
   export default Vue;
 }
