f434e1f147da78e48a6496233feac69b98183a11
Tamino Martinius <dev@zaku.eu>
2018-06-23 16:29:31 +0200

    Add sass and scss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,27 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
