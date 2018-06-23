e7dabd9d1c895e9d130491c0459e42edc6bf98f0
Tamino Martinius <dev@zaku.eu>
2018-06-23 11:15:14 +0200

    Add ts component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import vue from 'vue';
+import Hello from './components/Hello';
 import HelloVue from './components/Hello.vue';
 import HelloDecoratorVue from './components/HelloDecorator.vue';
 
@@ -7,14 +8,17 @@ const v = new vue({
   template: `
     <div>
         Name: <input v-model="name" type="text">
-        <h1>Hello Component</h1>
+        <h1>Hello TS Component</h1>
+        <hello :name="name" :initialEnthusiasm="5" />
+        <h1>Hello Vue Component</h1>
         <hello-vue :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Decorator Component</h1>
+        <h1>Hello Vue Decorator Component</h1>
         <hello-decorator-vue :name="name" :initialEnthusiasm="5" />
         </div>
     `,
   data: { name: 'World' },
   components: {
+    Hello,
     HelloVue,
     HelloDecoratorVue,
   },
