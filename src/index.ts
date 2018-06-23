b794792b3b399581a8d19b66ccfd1381087537e1
Tamino Martinius <dev@zaku.eu>
2018-06-23 11:19:38 +0200

    Update indentation

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ const v = new vue({
   el: '#app',
   template: `
     <div>
-        Name: <input v-model="name" type="text">
-        <h1>Hello TS Component</h1>
-        <hello :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Vue Component</h1>
-        <hello-vue :name="name" :initialEnthusiasm="5" />
-        <h1>Hello Vue Decorator Component</h1>
-        <hello-decorator-vue :name="name" :initialEnthusiasm="5" />
-        </div>
+      Name: <input v-model="name" type="text">
+      <h1>Hello TS Component</h1>
+      <hello :name="name" :initialEnthusiasm="5" />
+      <h1>Hello Vue Component</h1>
+      <hello-vue :name="name" :initialEnthusiasm="5" />
+      <h1>Hello Vue Decorator Component</h1>
+      <hello-decorator-vue :name="name" :initialEnthusiasm="5" />
+      </div>
     `,
   data: { name: 'World' },
   components: {
