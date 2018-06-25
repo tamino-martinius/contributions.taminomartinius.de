55bf0331c2f89585c3054f79d7c5ba9b8de07f2e
Tamino Martinius <dev@zaku.eu>
2018-06-25 23:37:13 +0200

    Start non decorator tsx version

--- src/components/HelloTsx.tsx
+++ src/components/HelloTsx.tsx
@@ -1,33 +1,34 @@
-// // The default way to create components
-// import vue from 'vue';
+// The default way to create components
+import vue from 'vue';
 
-// export default vue.extend({
-//   props: ['name', 'initialEnthusiasm'],
-//   data() {
-//     return {
-//       enthusiasm: this.initialEnthusiasm,
-//     };
-//   },
-//   methods: {
-//     increment() { this.enthusiasm += 1; },
-//     decrement() {
-//       if (this.enthusiasm > 1) {
-//         this.enthusiasm -= 1;
-//       }
-//     },
-//   },
-//   computed: {
-//     exclamationMarks(): string {
-//       return Array(this.enthusiasm + 1).join('!');
-//     },
-//   },
-//   render(h: Fun) {
-//     return (
-//       <div>
-//         <div>Hello { name }{ this.exclamationMarks }</div>
-//         <div onClick={ this.decrement }>-</div>
-//         <div onClick={ this.increment }>+</div>
-//       </div>
-//     );
-//   },
-// });
+export default vue.extend({
+  props: ['name', 'initialEnthusiasm'],
+  data() {
+    return {
+      enthusiasm: this.initialEnthusiasm,
+    };
+  },
+  methods: {
+    increment() { this.enthusiasm += 1; },
+    decrement() {
+      if (this.enthusiasm > 1) {
+        this.enthusiasm -= 1;
+      }
+    },
+  },
+  computed: {
+    exclamationMarks(): string {
+      return Array(this.enthusiasm + 1).join('!');
+    },
+  },
+  render() {
+    return (
+      <div>
+        Hello World
+        {/* <div>Hello { this.name }{ this.exclamationMarks }</div>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.increment}> +</button > */}
+      </div >
+    );
+  },
+});
