b0bb6c004ca9f386c81445cb5303ea1a66522d85
Tamino Martinius <dev@zaku.eu>
2018-06-25 23:28:50 +0200

    Add jsx namespace

---
+++ src/jsx.d.ts
@@ -0,0 +1,16 @@
+import Vue, { VNode } from 'vue'
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode { }
+    interface ElementClass extends Vue { }
+    interface ElementAttributesProperty {
+      $props: {}
+    }
+
+    interface IntrinsicElements {
+      div: any
+      button: any
+    }
+  }
+}
