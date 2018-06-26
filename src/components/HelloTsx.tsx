82bec0803b799c4fefad4f8e9e8476706bcba322
Tamino Martinius <dev@zaku.eu>
2018-06-26 07:02:14 +0200

    Comment non working parts

--- src/components/HelloTsx.tsx
+++ src/components/HelloTsx.tsx
@@ -8,25 +8,24 @@ export default vue.extend({
       enthusiasm: this.initialEnthusiasm,
     };
   },
-  methods: {
-    increment() { this.enthusiasm += 1; },
-    decrement() {
-      if (this.enthusiasm > 1) {
-        this.enthusiasm -= 1;
-      }
-    },
-  },
-  computed: {
-    exclamationMarks(): string {
-      return Array(this.enthusiasm + 1).join('!');
-    },
-  },
+  // methods: {
+  //   increment() { this.enthusiasm += 1; },
+  //   decrement() {
+  //     if (this.enthusiasm > 1) {
+  //       this.enthusiasm -= 1;
+  //     }
+  //   },
+  // },
+  // computed: {
+  //   exclamationMarks(): string {
+  //     return Array(this.enthusiasm + 1).join('!');
+  //   },
+  // },
   render() {
     return (
       <div>
-        Hello World
-        {/* <div>Hello { this.name }{ this.exclamationMarks }</div>
-        <button onClick={this.decrement}>-</button>
+        <div>Hello { this.name }</div>
+        {/* <button onClick={this.decrement}>-</button>
         <button onClick={this.increment}> +</button > */}
       </div >
     );
