d4209a0dc61e93a5bbd9f83eb0ad880497da2ef1
Tamino Martinius <dev@zaku.eu>
2018-07-16 11:41:00 +0200

    Load data and show stats

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,39 @@
 // This is an alternative way to define components using decorators
-import { Vue, Component, Prop } from 'vue-property-decorator';
-import { Data } from '../models/Data';
+import { Vue, Component } from 'vue-property-decorator';
+import Data from '../models/Data';
+import WeekDays from '../components/WeekDays';
+import {
+  Dict,
+  TimeStats,
+} from '../types';
+
+const data = new Data();
 
 @Component
-export default class App extends Vue {
-  @Prop() name: string = 'test';
-  @Prop() initialEnthusiasm: number = 10;
+export default class extends Vue {
+  stats: TimeStats | false = false;
+  repoStats: Dict<TimeStats> | false = false;
 
-  enthusiasm = this.initialEnthusiasm;
-
-  increment() {
-    this.enthusiasm += 1;
-  }
-  decrement() {
-    if (this.enthusiasm > 1) {
-      this.enthusiasm -= 1;
-    }
+  created() {
+    data.getStats().then(stats => this.stats = stats);
+    data.getRepoStats().then(stats => this.repoStats = stats);
   }
 
-  get exclamationMarks(): string {
-    return Array(this.enthusiasm + 1).join('!');
-  }
+  // data() {
+  //   return {
+  //     stats: undefined,
+  //     repoStats: undefined,
+  //   };
+  // }
 
   render() {
     return (
       <div>
-        <div>Hello {this.name}{this.exclamationMarks}</div>
-        <button onClick={this.decrement}>-</button>
-        <button onClick={this.increment}>+</button>
+        {this.stats ? (
+          <WeekDays stats={this.stats.weekDays} />
+        ) : (
+          <div>has no stats</div>
+        )}
       </div>
     );
   }
