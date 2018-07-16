83ac055454fdd0382889607753feb6f44d6fb3c0
Tamino Martinius <dev@zaku.eu>
2018-07-16 06:24:51 +0200

    Fetch data on create

--- src/models/Data.ts
+++ src/models/Data.ts
@@ -1,5 +1,13 @@
 const GITHUB_USER_LOGIN = 'tamino-martinius';
 
 export class Data {
+  constructor() {
+
+  }
+
+  async create() {
+    console.log(fetch(`/${ GITHUB_USER_LOGIN }`));
+  }
 }
+
 export default Data;
