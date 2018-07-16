d5232ae02a1a4d400cd6b73314bf0f722958dc01
Tamino Martinius <dev@zaku.eu>
2018-07-16 18:17:09 +0200

    Add total for each hour and sum

--- src/components/WeekDay.tsx
+++ src/components/WeekDay.tsx
@@ -3,16 +3,25 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 import {
   WeekDayStats,
 } from '../types';
+import Total from './Total';
 
 @Component
 export default class extends Vue {
   @Prop() stats!: WeekDayStats;
 
   render() {
+    const hours = [];
+    for (let hourNumber = 0; hourNumber < 24; hourNumber += 1) {
+      hours.push(<Total stats={this.stats.hours[hourNumber.toString()]} />);
+    }
     return (
-      <div>
-        {console.log(this.stats)}
-        Hello World
+      <div class="weekday">
+        <div class="weekday__sum">
+          <Total stats={this.stats} />
+        </div>
+        <div class="weekday__hours">
+          {hours}
+        </div>
       </div>
     );
   }
