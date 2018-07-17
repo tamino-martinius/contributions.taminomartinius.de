f21147f167b21f80e5be2276c74e95603a1684f2
Tamino Martinius <dev@zaku.eu>
2018-07-17 19:22:18 +0200

    Duplicate total nodes

--- src/components/WeekDay.tsx
+++ src/components/WeekDay.tsx
@@ -14,9 +14,11 @@ export default class extends Vue {
     for (let hourNumber = 0; hourNumber < 24; hourNumber += 1) {
       hours.push(<Total stats={this.stats.hours[hourNumber.toString()]} />);
     }
+    hours.push(...hours);
     return (
       <div class="weekday">
         <div class="weekday__sum">
+          <Total stats={this.stats} />
           <Total stats={this.stats} />
         </div>
         <div class="weekday__hours">
