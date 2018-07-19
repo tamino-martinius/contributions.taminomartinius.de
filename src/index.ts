fc02367a438b02055ad097f6c9c8ebb23cc002e5
Tamino Martinius <dev@zaku.eu>
2018-07-19 06:27:23 +0200

    Add max scroll comnstant

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from './components/App';
 import { Vue, Component } from 'vue-property-decorator';
 
+const MAX_SCROLL = 1_000;
+
 @Component({
   template: `<app />`,
   components: { App },
