033978ce816884b582b56a8e86590798570ef218
Tamino Martinius <dev@zaku.eu>
2018-07-22 11:57:29 +0200

    Add row component

---
+++ src/components/Row.tsx
@@ -0,0 +1,25 @@
+// This is an alternative way to define components using decorators
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+export enum RowType {
+  FULL = 'row--full',
+  FIRST_THIRD = 'row--first-third',
+  LAST_THIRD = 'row--last-third',
+}
+
+@Component
+export default class extends Vue {
+  @Prop() type!: RowType;
+
+  render() {
+    const classes = ['row', this.type || RowType.FULL];
+
+    return (
+      <div class={classes.join(' ')}>
+        {this.$slots.default}
+        {this.$slots.first}
+        {this.$slots.last}
+      </div>
+    );
+  }
+}
