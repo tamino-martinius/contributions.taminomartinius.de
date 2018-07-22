2703b143630a003bfd992f851c8b046a503b6736
Tamino Martinius <dev@zaku.eu>
2018-07-22 22:28:48 +0200

    Add bar component

---
+++ src/components/Bar.tsx
@@ -0,0 +1,23 @@
+// This is an alternative way to define components using decorators
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { DataPoint } from '@/types';
+export { DataPoint } from '@/types';
+
+@Component
+export default class extends Vue {
+  @Prop() sections!: DataPoint[];
+
+  render() {
+    const sections = this.sections.map(data => (
+      <div class="bar__section" style={{ '--color': `var(--${data.color})` }}>
+        {data.value}
+      </div>
+    ));
+
+    return (
+      <div class="bar">
+        {sections}
+      </div>
+    );
+  }
+}
