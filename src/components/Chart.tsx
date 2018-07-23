74cba5eed1d4d9230653e405ca1490a11541dc9c
Tamino Martinius <dev@zaku.eu>
2018-07-23 06:40:12 +0200

    Add chart type and classes

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -3,20 +3,23 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 import { Graph } from '@/types';
 import Card from '@/components/Card';
 
-export enum GraphType {
-  DISTINCT = 'graph--distinct',
-  STACKED = 'graph--stacked',
-  COMPARE = 'graph--compare',
+export enum ChartType {
+  DISTINCT = 'chart--distinct',
+  STACKED = 'chart--stacked',
+  COMPARE = 'chart--compare',
 }
 
 @Component
 export default class extends Vue {
   @Prop() graphs!: Graph[];
   @Prop() title!: string;
+  @Prop() type!: ChartType;
 
   render() {
+    const classes = ['chart', this.type || ChartType.DISTINCT];
+
     return (
-      <Card title={this.title}>
+      <Card class={classes.join(' ')} title={this.title}>
         TBD
       </Card>
     );
