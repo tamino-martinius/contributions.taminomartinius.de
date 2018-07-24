e89a65b80c32c07805d8ff7225d833a43637f62b
Tamino Martinius <dev@zaku.eu>
2018-07-24 19:52:13 +0200

    Change bar to be either horizontal or vertical

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -3,21 +3,30 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 import { DataPoint } from '@/types';
 export { DataPoint } from '@/types';
 
+export enum BarType {
+  HORIZONTAL = 'bar--horizontal',
+  VERTICAL = 'bar--vertical',
+}
+
 @Component
 export default class extends Vue {
   @Prop() sections!: DataPoint[];
+  @Prop() type!: BarType | undefined;
 
   render() {
+    const type = this.type || BarType.HORIZONTAL;
+
     const sections = this.sections.map(data => (
-      <div class="bar__section" style={{ '--color': `var(--${data.color})` }}>
+      <div class={['bar__section', type]} style={{ '--color': `var(--${data.color})` }} >
         {data.value.toLocaleString()}
       </div>
     ));
 
-    const gridTemplateColumns = this.sections.map(data => `${data.value}fr`).join(' ');
+    const weights = this.sections.map(data => `${data.value}fr`).join(' ');
+    const template = type === BarType.HORIZONTAL ? 'gridTemplateColumns' : 'gridTemplateRows';
 
     return (
-      <div class="bar" style={{ gridTemplateColumns }}>
+      <div class="bar" style={{ [template]: weights }}>
         {sections}
       </div>
     );
