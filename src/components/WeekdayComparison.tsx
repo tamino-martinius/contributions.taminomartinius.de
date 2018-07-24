328ea5eb968b300093a26616bf41cc0a1066b85d
Tamino Martinius <dev@zaku.eu>
2018-07-24 19:52:51 +0200

    Add legend to footer

--- src/components/WeekdayComparison.tsx
+++ src/components/WeekdayComparison.tsx
@@ -2,6 +2,7 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import Card from '@/components/Card';
 import { CommitSplit, Dict, WeekDayStats } from '@/types';
+import Legend from '@/components/Legend';
 
 @Component
 export default class extends Vue {
@@ -9,9 +10,15 @@ export default class extends Vue {
 
   render() {
 
+    const sections: DataPoint[] = [
+      { color: 'color-open', title: 'Open Source', value: 'nothing' },
+      { color: 'color-closed', title: 'Private', value: 'nothing' },
+    ];
+
     return (
       <Card title="Weekday Comparison" class="weekday-comparison">
 
+        <Legend class="weekday-comparison__legend" sections={sections} slot="footer" />
       </Card>
     );
   }
