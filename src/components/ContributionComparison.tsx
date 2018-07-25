a53b5947e61593ac7e90902e3bb22e8f09f5b1c7
Tamino Martinius <dev@zaku.eu>
2018-07-25 13:34:03 +0200

    Add methods to create path for arc

--- src/components/ContributionComparison.tsx
+++ src/components/ContributionComparison.tsx
@@ -8,6 +8,29 @@ import { CommitSplit, DataPoint, Counts } from '@/types';
 export default class extends Vue {
   @Prop() counts!: CommitSplit<Counts>;
 
+  polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
+    const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians)),
+    };
+  }
+
+  describeArc(x: number, y: number, radius: number, startAngle: number, endAngle: number) {
+    const start = this.polarToCartesian(x, y, radius, endAngle);
+    const end = this.polarToCartesian(x, y, radius, startAngle);
+
+    const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+
+    const d = [
+      'M', start.x, start.y,
+      'A', radius, radius, 0, arcSweep, 0, end.x, end.y,
+    ].join(' ');
+
+    return d;
+  }
+
   render() {
     const valueOpen = this.counts.open.commitCount / this.counts.sum.commitCount;
     const valueClosed = this.counts.closed.commitCount / this.counts.sum.commitCount;
