d48952ec37d1fa1490072a4ea502708048e41e4b
Tamino Martinius <dev@zaku.eu>
2018-07-25 22:27:29 +0200

    Add component for timeline

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,60 @@
+// This is an alternative way to define components using decorators
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import Chart, { ChartType } from '@/components/Chart';
+import { Dict, Graph, Counts, CommitSplit } from '@/types';
+
+@Component
+export default class extends Vue {
+  @Prop() dates!: CommitSplit<Dict<Counts>>;
+
+  render() {
+    const openValues: number[] = [];
+    const closedValues: number[] = [];
+
+    const firstDate = new Date('2013');
+    const lastDate = new Date();
+    let date = new Date(firstDate);
+    while (date < lastDate) {
+      const key = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+      const openCount = this.dates.open[key];
+      const closedCount = this.dates.closed[key];
+      openValues.push(openCount ? openCount.commitCount : 0);
+      closedValues.push(closedCount ? closedCount.commitCount : 0);
+      date.setDate(date.getDate() + 1);
+    }
+
+    const openValue = openValues.reduce((sum, value) => sum + value, 0);
+    const closedValue = closedValues.reduce((sum, value) => sum + value, 0);
+
+    const openGraph: Graph = {
+      title: 'Open Source',
+      color: 'color-open',
+      value: openValue,
+      values: openValues,
+    };
+    const closedGraph: Graph = {
+      title: 'Private',
+      color: 'color-closed',
+      value: closedValue,
+      values: closedValues,
+    };
+
+    const graphs = [openGraph, closedGraph];
+    const xLabels: string[] = [];
+    date = new Date(firstDate);
+    while (date < lastDate) {
+      xLabels.push(`${date.getFullYear()}-${date.getMonth() + 1}`);
+      date.setMonth(date.getMonth() + 6);
+    }
+
+    return (
+      <Chart
+        class="timeline"
+        title="Timeline"
+        type={ChartType.COMPARE}
+        graphs={graphs}
+        xLabels={xLabels}
+      />
+    );
+  }
+}
