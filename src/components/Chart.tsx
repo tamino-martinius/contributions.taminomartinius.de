c5d05a49d1902cb096bb8e4fa8ef0fa8c0a795db
Tamino Martinius <dev@zaku.eu>
2018-07-26 13:10:08 +0200

    Add stacked graphs implementation

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -4,7 +4,7 @@ import { Graph } from '@/types';
 import Card from '@/components/Card';
 import ButtonGroup from '@/components/ButtonGroup';
 import Legend from '@/components/Legend';
-import { scaleLinear, area, line, curveBasis } from 'd3';
+import { scaleLinear, area, line, curveBasis, values } from 'd3';
 
 export enum ChartType {
   DISTINCT = 'chart--distinct',
@@ -21,17 +21,31 @@ export default class extends Vue {
   @Prop() title!: string;
   @Prop() xLabels!: string[];
   @Prop() type!: ChartType | undefined;
+  currentType = this.type || ChartType.STACKED;
 
   typeChangeHandler(type: ChartType) {
-    console.log(type);
+    this.currentType = type;
   }
 
   render() {
-    const type = this.type || ChartType.STACKED;
-    const classes = ['chart', type];
+    const classes = ['chart', this.currentType];
+    const graphs: Graph[] = this.graphs.map(graph => ({
+      title: graph.title,
+      color: graph.color,
+      value: graph.value,
+      values: graph.values.slice(0),
+    }));
 
-    const xMax = Math.max(...this.graphs.map(graph => graph.values.length));
-    const yMax = Math.max(...this.graphs.map(graph =>
+    const xMax = Math.max(...graphs.map(graph => graph.values.length));
+    let y0 = Array.from({ length: xMax }).map(() => 0);
+    if (this.currentType === ChartType.STACKED) {
+      for (let i = 1; i < graphs.length; i += 1) {
+        graphs[i].values = graphs[i].values.map(
+          (y, x) => y + graphs[i - 1].values[x] || 0,
+        );
+      }
+    }
+    const yMax = Math.max(...graphs.map(graph =>
       Math.max(...graph.values),
     ));
 
@@ -40,37 +54,35 @@ export default class extends Vue {
     const yScaleTop = scaleLinear().domain([0, yMax]).range([CHART_HEIGHT / 2, 0]);
     const yScaleBottom = scaleLinear().domain([0, yMax]).range([CHART_HEIGHT / 2, CHART_HEIGHT]);
 
-    const y0 = Array.from({ length: xMax }).map(() => 0);
-
     const createPath = (values: number[], index: number) => {
       let yScale = yScaleComplete;
-      if (type === ChartType.COMPARE) {
+      if (this.currentType === ChartType.COMPARE) {
         yScale = index % 2 === 1 ? yScaleTop : yScaleBottom;
       }
       return line<number>()
         .x((d, i) => xScale(i))
-        .y((d, i) => yScale(y0[i] + d))
+        .y((d, i) => yScale(d))
         .curve(curveBasis)(values)
         ;
     };
 
     const createArea = (values: number[], index: number) => {
       let yScale = yScaleComplete;
-      if (type === ChartType.COMPARE) {
+      if (this.currentType === ChartType.COMPARE) {
         yScale = index % 2 === 1 ? yScaleTop : yScaleBottom;
       }
       return area<number>()
         .x((d, i) => xScale(i))
-        .y0((d, i) => yScale(y0[i]))
-        .y1((d, i) => yScale(y0[i] + d))
+        .y0((d, i) => yScale(y0[i] || 0))
+        .y1((d, i) => yScale(d))
         .curve(curveBasis)(values)
         ;
     };
 
-    const graphs: JSX.Element[] = [];
-    for (let i = 0; i < this.graphs.length; i += 1) {
-      const graph = this.graphs[i];
-      graphs.push(
+    const graphElements: JSX.Element[] = [];
+    for (let i = 0; i < graphs.length; i += 1) {
+      const graph = graphs[i];
+      graphElements.push(
         <path
           class="chart__area"
           d={createArea(graph.values, i)}
@@ -82,12 +94,14 @@ export default class extends Vue {
           style={{ '--color': `var(--${graph.color})` }}
         />,
       );
+      if (this.currentType === ChartType.STACKED) {
+        y0 = graph.values;
+      }
     }
-
     let divider: JSX.Element | undefined;
     let typeToggle: JSX.Element | undefined;
 
-    if (type === ChartType.COMPARE) {
+    if (this.currentType === ChartType.COMPARE) {
       divider = (
         <path
           class="chart__divider"
@@ -119,7 +133,7 @@ export default class extends Vue {
               width={`${CHART_WIDTH}px`}
               height={`${CHART_HEIGHT}px`}
             >
-              {graphs}
+              {graphElements}
               {divider}
             </svg>
           </div>
