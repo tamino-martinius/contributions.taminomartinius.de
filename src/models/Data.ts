cff5ee7da3adebf221f8ca405c6b442846fc6125
Tamino Martinius <dev@zaku.eu>
2018-07-26 06:47:53 +0200

    Change data source depending on env

--- src/models/Data.ts
+++ src/models/Data.ts
@@ -1,17 +1,17 @@
 import {
-  Dict,
   StatsData,
 } from '@/types';
 
 const GITHUB_USER_LOGIN = 'tamino-martinius';
 
+declare const DEBUG_MODE: boolean;
 export class Data {
   constructor() {
 
   }
 
   async getStats(): Promise<StatsData> {
-    const response = await fetch(`/${GITHUB_USER_LOGIN}.json`);
+    const response = await fetch(`/${DEBUG_MODE ? 'dev' : GITHUB_USER_LOGIN}.json`);
     const data: StatsData = await response.json();
     return data;
   }
