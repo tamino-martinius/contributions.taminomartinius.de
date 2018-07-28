d73cf3a85a1a8fa5c8b02524addb00a5f4c60c8c
Tamino Martinius <dev@zaku.eu>
2018-07-28 12:11:07 +0200

    Change cards to be faded in

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,29 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import Util from '@/models/Util';
+
+const MIN_SHOW_DISTANCE_DURATION = 100;
 
 @Component
-export default class extends Vue {
+export default class Card extends Vue {
+  static lastShow = Date.now();
+
   @Prop() title!: string;
+  isVisible = false;
+
+  async show() {
+    const waitDuration = MIN_SHOW_DISTANCE_DURATION + Card.lastShow - Date.now();
+    if (waitDuration > 0) {
+      Card.lastShow = Date.now() + waitDuration;
+      await Util.waitFor(waitDuration);
+    } else {
+      Card.lastShow = Date.now();
+    }
+    this.isVisible = true;
+  }
+
+  mounted() {
+    this.show();
+  }
 
   render() {
     const footer = this.$slots.footer ? (
@@ -12,7 +33,7 @@ export default class extends Vue {
     ) : undefined;
 
     return (
-      <div class="card">
+      <div class={['card', this.isVisible ? 'card--visible' : '']}>
         <div class="card__title">
           <h2>
             {this.title}
